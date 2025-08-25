808380f9a70fd915a2e9838e9c5818c2da0c8e18
MuraliKrishnan <164983722+Muralikrishnan08@users.noreply.github.com>
2025-08-25 14:08:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,50 +19,51 @@ function playAudio(keyPressed) {
             document.querySelector('.w').style.color = "green";
             setTimeout(function()
             {document.querySelector('.w').style.color="blueviolet"},100);
-            var audio = new Audio("./sounds/tom-1.mp3");
+            var audio = new Audio("tom-1.mp3");
             audio.play();
             break;
         case 'a':
             document.querySelector('.a').style.color = "green";
             setTimeout(function()
             {document.querySelector('.a').style.color="blueviolet"},100);
-            var audio = new Audio("./sounds/tom-2.mp3");
+            var audio = new Audio("tom-2.mp3");
             audio.play();
             break;
         case 's':
             document.querySelector('.s').style.color = "green";
             setTimeout(function()
             {document.querySelector('.s').style.color="blueviolet"},100);
-            var audio = new Audio("./sounds/tom-3.mp3");
+            var audio = new Audio("tom-3.mp3");
             audio.play();
             break;
         case 'd':
             document.querySelector('.d').style.color = "green";
             setTimeout(function()
             {document.querySelector('.d').style.color="blueviolet"},100);
-            var audio = new Audio("./sounds/tom-4.mp3");
+            var audio = new Audio("tom-4.mp3");
             audio.play();
             break;
         case 'j':
             document.querySelector('.j').style.color = "green";
             setTimeout(function()
             {document.querySelector('.j').style.color="blueviolet"},100);
-            var audio = new Audio("./sounds/snare.mp3");
+            var audio = new Audio("snare.mp3");
             audio.play();
             break;
         case 'k':
             document.querySelector('.k').style.color = "green";
             setTimeout(function()
             {document.querySelector('.k').style.color="blueviolet"},100);
-            var audio = new Audio("./sounds/kick-bass.mp3");
+            var audio = new Audio("kick-bass.mp3");
             audio.play();
             break;
         case 'l':
             document.querySelector('.l').style.color = "green";
             setTimeout(function()
             {document.querySelector('.l').style.color="blueviolet"},100);
-            var audio = new Audio("./sounds/crash.mp3");
+            var audio = new Audio("crash.mp3");
             audio.play();
             break;
     }
 }
+
